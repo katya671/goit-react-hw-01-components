da89c9f015af395ad81979c35a8ea0f84d48c6d7
Толочко Катерина <bantiktokinpakistan@gmail.com>
2023-08-02 22:07:06 +0300

    Format document

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,33 +1,27 @@
 import css from './FriendList.module.css';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
-import FriendListItem from "../FriendListItem/FriendListItem";
+import FriendListItem from '../FriendListItem/FriendListItem';
 
-const FriendList = ({
-  friends,
-  
-}) => (
-    <ul className={css['friend-list']}>
-        {friends.map(({ avatar, name, isOnline, id }) => (
-          <li className={css.item} key={id}>
-            <FriendListItem
-              avatar={avatar}
-              name={name}
-              isOnline={isOnline}
-            />
-          </li>
-        ))}
-    </ul>
+const FriendList = ({ friends }) => (
+  <ul className={css['friend-list']}>
+    {friends.map(({ avatar, name, isOnline, id }) => (
+      <li className={css.item} key={id}>
+        <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+      </li>
+    ))}
+  </ul>
 );
 
 export default FriendList;
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.shape({
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
-    id: PropTypes.number.isRequired,
-  })).isRequired,
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,22 +1,19 @@
 import css from './FriendListItem.module.css';
 
-const FriendListItem = ({
-  avatar, name, isOnline,
-  
-}) => (
-    <>
-        {isOnline ? (
-            <span className={css.status} style={{ color: '#67ac5a' }}>
-            &#8226;
-            </span>
-        ) : (
-            <span className={css.status} style={{ color: '#ed5d5a' }}>
-            &#8226;
-            </span>
-        )}
-        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-        <p className={css.name}>{name}</p>
-    </>
+const FriendListItem = ({ avatar, name, isOnline }) => (
+  <>
+    {isOnline ? (
+      <span className={css.status} style={{ color: '#67ac5a' }}>
+        &#8226;
+      </span>
+    ) : (
+      <span className={css.status} style={{ color: '#ed5d5a' }}>
+        &#8226;
+      </span>
+    )}
+    <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+    <p className={css.name}>{name}</p>
+  </>
 );
 
 export default FriendListItem;

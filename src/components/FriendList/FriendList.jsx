55bb4ddcf04a7d97164f032b536e03009ac1003f
Толочко Катерина <bantiktokinpakistan@gmail.com>
2023-08-02 21:27:42 +0300

    Task 3: FriendList and FriendListItem - Add files

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,32 @@
+import css from './FriendList.module.css';
+import PropTypes from "prop-types";
+
+import FriendListItem from "../FriendListItem/FriendListItem";
+
+const FriendList = ({
+  friends,
+  
+}) => (
+    <ul className={css['friend-list']}>
+        {friends.map(({ avatar, name, isOnline, id }) => (
+          <li className={css.item} key={id}>
+            <FriendListItem
+              avatar={avatar}
+              name={name}
+              isOnline={isOnline}
+            />
+          </li>
+        ))}
+    </ul>
+);
+
+export default FriendList;
+
+FriendList.propTypes = {
+  friends: PropTypes.shape({
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
